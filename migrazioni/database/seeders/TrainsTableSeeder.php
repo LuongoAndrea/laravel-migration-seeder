fc74dd06011ff9f7dc8d800ef3677815b18adc19
LuongoAndrea <andrea.luongo2002@gmail.com>
2022-12-22 00:45:45 +0100

    faker

--- migrazioni/database/seeders/TrainsTableSeeder.php
+++ migrazioni/database/seeders/TrainsTableSeeder.php
@@ -16,18 +16,18 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        // for ($i=0; $i < 10; $i++) { 
+        for ($i = 0; $i < 10; $i++) {
 
-        // }
-        $newtrain = new Train();
-        $newtrain->Azienda = 'a';
-        $newtrain->Stazione_di_partenza = 'a';
-        $newtrain->Stazione_di_arrivo = 'a';
-        $newtrain->Orario_di_partenza = '12/03/12';
-        $newtrain->Orario_di_arrivo = '12/03/12';
-        $newtrain->Codice_Treno = 'a';
-        $newtrain->Numero_Carrozze = '1';
-        $newtrain->stato = 'In orario';
 
+            $newtrain = new Train();
+            $newtrain->Azienda = $faker->company();
+            $newtrain->Stazione_di_partenza = $faker->city();
+            $newtrain->Stazione_di_arrivo = $faker->city();
+            $newtrain->Orario_di_partenza = 12 / 12 / 12;
+            $newtrain->Orario_di_arrivo = 12 / 12 / 12;
+            $newtrain->Codice_Treno = $faker->numerify('train-####');
+            $newtrain->Numero_Carrozze = $faker->randomDigitNot(0);
+            $newtrain->stato = 'In orario';
+        }
     }
 }
